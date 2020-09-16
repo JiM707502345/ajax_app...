dbaf0adc08a81ed778ab5955b26f580390ab2910
sugimotojin <tatoonashi0203@icloud.com>
2020-09-16 16:48:53 +0900

    Basic認証を導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :basic_auth
+
   def index
     @posts = Post.all.order(id: "DESC")
   end
@@ -18,4 +20,9 @@ class PostsController < ApplicationController
     render json: { post: item }
   end
 
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    end
+  end
 end
